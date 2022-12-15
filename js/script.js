b76fbe0e17bd4f0533667c4c20b918f0db774fed
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-12-15 11:55:23 -0500

    4324

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/Unit-6-03-JS/sw.js", {
+    scope: "/Unit-6-03-JS/",
   })
 }
 
@@ -12,27 +12,28 @@ if (navigator.serviceWorker) {
 
 const weather2 = async (URLAddress) => {
   try {
-    const request = await fetch(URLAddress)
-    const jsonData = await request.json()
-    var temp1 = jsonData.main.temp
-    var temp2 = 0
-    const feeling = jsonData.weather[0]
-    const image = feeling.icon
-    temp2 = temp1 - 273.15
-
-    console.log(jsonData.weather)
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    var temp1 = jsonData.main.temp;
+    var temp2 = 0;
+    const feeling = jsonData.weather[0];
+    const image = feeling.icon;
+    temp2 = temp1 - 273.15;
+    
+    console.log(jsonData.weather);
     document.getElementById("Image").innerHTML =
       "<img src='http://openweathermap.org/img/wn/" +
       image +
-      "@2x.png' alt='Icon' width='10%'><br><h5>"
-    ;(">")
+      "@2x.png' alt='Icon' width='10%'><br><h5>";
+    (">");
+
 
     document.getElementById("TheWeather").innerHTML =
-      "The Weather Right now is " + temp2.toFixed(2) + " °C"
+      "The Weather Right now is " + temp2.toFixed(2) + " °C";
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 weather2(
   "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-)
+);
